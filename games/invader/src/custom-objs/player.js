488379d87ae4789a29d8945c2b9a994f51829acc
jjcapellan <soycape@hotmail.com>
2024-01-06 14:17:01 +0100

    Invader: add player move controls

--- games/invader/src/custom-objs/player.js
+++ games/invader/src/custom-objs/player.js
@@ -1,3 +1,5 @@
+const PLAYER_SPEED = 120;
+
 export default class Player extends Phaser.Physics.Arcade.Sprite {
 
     constructor(scene, x, y) {
@@ -7,14 +9,24 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         this.anims.create({ key: "player_idle", frames: this.anims.generateFrameNames("atlas", { prefix: "Player-", end: 5 }), repeat: -1 });
         this.play("player_idle");
 
-        // Physics configured only to allow collisions
+        // Physics
         this.scene.physics.add.existing(this);
         this.enableBody();
-        this.setDirectControl();
+        this.setDragX(PLAYER_SPEED * 1.8);
+
+        // Controls
+        this.leftKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.rightKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
     }
 
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
+        if(this.leftKey.isDown){
+            this.setVelocityX(-PLAYER_SPEED);
+        }
+        if(this.rightKey.isDown){
+            this.setVelocityX(PLAYER_SPEED);
+        }
     }
 
 } // End class Player
