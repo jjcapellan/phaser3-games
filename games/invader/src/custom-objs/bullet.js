d255553758bb3d6eb0a0254a6c003ef3a24c4fa3
jjcapellan <soycape@hotmail.com>
2024-01-10 15:23:04 +0100

    Invader: add : Bullet.reset()

--- games/invader/src/custom-objs/bullet.js
+++ games/invader/src/custom-objs/bullet.js
@@ -19,10 +19,14 @@ export default class Bullet extends Phaser.Physics.Arcade.Image {
 
     preUpdate(time, delta) {
         if (this.y > this.scene.scale.height || this.y < 0) {
-            this.body.reset(-1000, 1);
-            this.setActive(false);
-            this.setVisible(false);
+            this.reset();
         }
+    }
+
+    reset() {
+        this.body.reset(-1000, 1);
+        this.setActive(false);
+        this.setVisible(false);
     }
 
     shoot(x, y) {
