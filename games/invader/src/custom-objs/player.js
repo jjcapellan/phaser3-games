87749100fdd1a4edb2658687f8279c65196d7676
jjcapellan <soycape@hotmail.com>
2024-01-12 21:52:00 +0100

    Invader: fix : player can shoot after hit

--- games/invader/src/custom-objs/player.js
+++ games/invader/src/custom-objs/player.js
@@ -6,6 +6,8 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y, "atlas", "Player-0");
 
+        this.isAlive = true;
+
         // Texture animation
         this.play("player_idle");
 
@@ -25,6 +27,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
     preUpdate(time, delta) {
         super.preUpdate(time, delta);
+        if(!this.isAlive) return;
         if (Phaser.Input.Keyboard.JustDown(this.shootKey) && !this.bullet.active) {
             this.chain(["player_shoot", "player_idle"]);
             this.stop();
@@ -48,6 +51,7 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         this.body.setAngularVelocity(Phaser.Math.Between(-30, 30));
         this.setCollideWorldBounds(true, 0, 0, true);
         this.play("player_hit");
+        this.isAlive = false;
     }
 
 } // End class Player
